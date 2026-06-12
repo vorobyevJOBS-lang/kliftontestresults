27bc937fdef525d22b9efa4d56dfce7798802df8
vorobyevJOBS-lang <vorobyev.arttech@gmail.com>
2026-06-12 14:28:56 +0700

    Update roles.js

--- src/roles.js
+++ src/roles.js
@@ -1,12 +1,38 @@
 export const ROLE_PROFILES = {
-manager: {
-name: “Менеджер по продажам”,
+director: {
+name: “Управляющий”,
 weights: {
+LEADERSHIP: 10,
+RESPONSIBILITY: 10,
+STRATEGIC: 10,
+ANALYTICAL: 9,
+PROBLEM_SOLVING: 9,
+EMOTIONAL_STABILITY: 8,
+ORGANIZATION: 8
+}
+},
+
+salesDirector: {
+name: “Руководитель отдела продаж”,
+weights: {
+LEADERSHIP: 10,
 COMMUNICATION: 10,
 INFLUENCE: 10,
+PERSISTENCE: 9,
 SELF_ASSURANCE: 9,
-ACHIEVER: 8,
-LEADERSHIP: 6
+ANALYTICAL: 7
+}
+},
+
+productLead: {
+name: “Руководитель продукта”,
+weights: {
+DEVELOPER: 10,
+LEADERSHIP: 9,
+LEARNER: 9,
+ANALYTICAL: 8,
+ORGANIZATION: 8,
+STRATEGIC: 8
 }
 },
 
@@ -17,7 +43,53 @@ EMPATHY: 10,
 DEVELOPER: 10,
 COMMUNICATION: 9,
 LEARNER: 8,
-RESPONSIBILITY: 8
+CUSTOMER_FOCUS: 8,
+EMOTIONAL_STABILITY: 7
+}
+},
+
+salesManager: {
+name: “Менеджер по продажам”,
+weights: {
+COMMUNICATION: 10,
+INFLUENCE: 10,
+SELF_ASSURANCE: 9,
+PERSISTENCE: 9,
+ACHIEVEMENT: 8,
+NEGOTIATION: 8
+}
+},
+
+bookingManager: {
+name: “Менеджер записи”,
+weights: {
+COMMUNICATION: 10,
+RESPONSIBILITY: 10,
+ORGANIZATION: 9,
+CUSTOMER_FOCUS: 9,
+DISCIPLINE: 8
+}
+},
+
+supervisor: {
+name: “Супервайзер”,
+weights: {
+LEADERSHIP: 10,
+RESPONSIBILITY: 10,
+ANALYTICAL: 8,
+TEAMWORK: 8,
+ORGANIZATION: 8
+}
+},
+
+promoter: {
+name: “Промоутер”,
+weights: {
+COMMUNICATION: 10,
+SELF_ASSURANCE: 10,
+PERSISTENCE: 9,
+INFLUENCE: 8,
+NEGOTIATION: 7
 }
 },
 
@@ -25,21 +97,23 @@ administrator: {
 name: “Администратор”,
 weights: {
 RESPONSIBILITY: 10,
-ORGANIZATION: 10,
-DISCIPLINE: 9,
-TEAMWORK: 8,
-EMPATHY: 7
+COMMUNICATION: 10,
+CUSTOMER_FOCUS: 10,
+ORGANIZATION: 9,
+EMPATHY: 8,
+DISCIPLINE: 8
 }
 },
 
-leader: {
-name: “Руководитель”,
+tutor: {
+name: “Тьютор”,
 weights: {
-LEADERSHIP: 10,
-STRATEGIC: 10,
-SELF_ASSURANCE: 9,
-RESPONSIBILITY: 8,
-ACHIEVER: 8
+EMPATHY: 10,
+DEVELOPER: 10,
+RESPONSIBILITY: 9,
+COMMUNICATION: 9,
+PROBLEM_SOLVING: 8,
+CUSTOMER_FOCUS: 8
 }
 }
 };
