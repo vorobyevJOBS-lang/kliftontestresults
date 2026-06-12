205b9b7468ac1c897fe5d9242197357a7feaaaee
vorobyevJOBS-lang <vorobyev.arttech@gmail.com>
2026-06-12 16:17:39 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import App from "./App.jsx";
+import Admin from "./Admin.jsx";
+
+const path = window.location.pathname;
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
-    <App />
+
+    {path === "/admin"
+      ? <Admin />
+      : <App />}
+
   </React.StrictMode>
 );
